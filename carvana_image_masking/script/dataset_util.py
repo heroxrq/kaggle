477b434384d5b2da4f88b8c3adddca5e795d8664
xierenqiang <840456819@qq.com>
2017-08-18 01:22:07 +0800

    refactor dataset_util(add hue_sat_val data augmentation) and unet

--- carvana_image_masking/script/dataset_util.py
+++ carvana_image_masking/script/dataset_util.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import os
 from PIL import Image
@@ -13,52 +14,94 @@ def load_img_array(image_name, grayscale=False, target_size=None):
     return img_array
 
 
-def train_data_generator(data_dir, mask_dir, images, batch_size, target_size=None, augment=False, seed=1):
-    data_gen_args = dict(rotation_range=5,
-                         width_shift_range=0.05,
-                         height_shift_range=0.05,
-                         horizontal_flip=True)
-    image_data_gen = ImageDataGenerator(**data_gen_args)
-    mask_data_gen = ImageDataGenerator(**data_gen_args)
+def random_hue_saturation_value(image,
+                                hue_shift_limit=(-50, 50),
+                                sat_shift_limit=(-5, 5),
+                                val_shift_limit=(-15, 15),
+                                u=0.5):
 
+    if np.random.random() < u:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(image)
+        hue_shift = np.random.uniform(hue_shift_limit[0], hue_shift_limit[1])
+        h = cv2.add(h, hue_shift)
+        sat_shift = np.random.uniform(sat_shift_limit[0], sat_shift_limit[1])
+        s = cv2.add(s, sat_shift)
+        val_shift = np.random.uniform(val_shift_limit[0], val_shift_limit[1])
+        v = cv2.add(v, val_shift)
+        image = cv2.merge((h, s, v))
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+    return image
+
+
+def random_transform(image,
+                     mask,
+                     seed,
+                     rotation_range=5,
+                     width_shift_range=0.05,
+                     height_shift_range=0.05,
+                     horizontal_flip=True,
+                     u=0.5):
+    if np.random.random() < u:
+        data_gen_args = dict(rotation_range=rotation_range,
+                             width_shift_range=width_shift_range,
+                             height_shift_range=height_shift_range,
+                             horizontal_flip=horizontal_flip)
+        image_data_gen = ImageDataGenerator(**data_gen_args)
+        mask_data_gen = ImageDataGenerator(**data_gen_args)
+
+        image = image_data_gen.random_transform(image, seed)
+        mask = mask_data_gen.random_transform(mask, seed)
+
+    return image, mask
+
+
+def data_augmentation(image, mask, seed):
+    image = random_hue_saturation_value(image)
+    image, mask = random_transform(image, mask, seed)
+    return image, mask
+
+
+def train_data_generator(data_dir, mask_dir, images, batch_size, target_size=None, augment=False, seed=1):
     batch_cnt = 0
     while True:
         idx = np.random.choice(len(images), batch_size)
-        imgs = []
-        labels = []
+        image_batch = []
+        mask_batch = []
         for i in idx:
             # images
             image_name = data_dir + "/" + images[i]
-            img_array = load_img_array(image_name, grayscale=False, target_size=target_size)
-            if augment:
-                img_array = image_data_gen.random_transform(img_array, seed + batch_cnt)
-            imgs.append(img_array)
+            image_array = load_img_array(image_name, grayscale=False, target_size=target_size)
 
             # masks
             mask_name = mask_dir + "/" + images[i].split(".")[0] + '_mask.gif'
             mask_array = load_img_array(mask_name, grayscale=True, target_size=target_size)
+
             if augment:
-                mask_array = mask_data_gen.random_transform(mask_array, seed + batch_cnt)
-            labels.append(mask_array)
+                image_array, mask_array = data_augmentation(image_array, mask_array, seed + batch_cnt)
+
+            image_batch.append(image_array)
+            mask_batch.append(mask_array)
 
             batch_cnt += 1
-        imgs = np.array(imgs)
-        labels = np.array(labels)
-        yield imgs, labels
+        image_batch = np.array(image_batch, np.float32)
+        mask_batch = np.array(mask_batch, np.float32)
+        yield image_batch, mask_batch
 
 
 def test_data_generator(data_dir, images, batch_size, target_size=None):
     for base in xrange(0, len(images), batch_size):
-        imgs = []
+        image_batch = []
         for offset in xrange(batch_size):
             idx = base + offset
             if idx >= len(images):
                 break
             image_name = data_dir + "/" + images[idx]
-            img_array = load_img_array(image_name, grayscale=False, target_size=target_size)
-            imgs.append(img_array)
-        imgs = np.array(imgs)
-        yield imgs
+            image_array = load_img_array(image_name, grayscale=False, target_size=target_size)
+            image_batch.append(image_array)
+        image_batch = np.array(image_batch, np.float32)
+        yield image_batch
 
 
 def image_resize(in_dir, out_dir, resized_width, resized_height, format=None, save_postfix=None):
@@ -76,7 +119,7 @@ def image_resize(in_dir, out_dir, resized_width, resized_height, format=None, sa
 
 def resize_all_images():
     image_resize(TRAIN_DIR, RESIZED_TRAIN_DIR, RESIZED_WIDTH, RESIZED_HEIGHT)
-    image_resize(TRAIN_MASKS_DIR, RESIZED_TRAIN_MASKS_DIR, RESIZED_WIDTH, RESIZED_HEIGHT)
+    # image_resize(TRAIN_MASKS_DIR, RESIZED_TRAIN_MASKS_DIR, RESIZED_WIDTH, RESIZED_HEIGHT)
     image_resize(TEST_DIR, RESIZED_TEST_DIR, RESIZED_WIDTH, RESIZED_HEIGHT)
 
 
--- carvana_image_masking/script/my_unet.py
+++ carvana_image_masking/script/my_unet.py
@@ -7,73 +7,13 @@ from keras.layers.pooling import MaxPooling2D
 from keras.models import Model
 
 
-# class UNet(object):
-#     def __init__(self, layers, input_shape, filters=32, num_classes=1):
-#         self.__layers = layers
-#         self.__input_shape = input_shape
-#         self.__filters = filters
-#         self.__num_classes = num_classes
-#
-#     def __down(self, input_layer, filters, kernel_size=(3, 3), pool=True):
-#         conv1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same')(input_layer)
-#         bn1 = BatchNormalization()(conv1)
-#         act1 = Activation('relu')(bn1)
-#         conv2 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same')(act1)
-#         bn2 = BatchNormalization()(conv2)
-#         residual = Activation('relu')(bn2)
-#         if pool:
-#             max_pooling = MaxPooling2D(pool_size=(2, 2))(residual)
-#             return max_pooling, residual
-#         else:
-#             return residual
-#
-#     def __up(self, residual, input_layer, filters, kernel_size=(3, 3)):
-#         up_sampling = UpSampling2D(size=(2, 2))(input_layer)
-#         conv0 = Conv2D(filters=filters, kernel_size=(2, 2), padding='same')(up_sampling)
-#         bn0 = BatchNormalization()(conv0)
-#         up_conv = Activation('relu')(bn0)
-#         concat = Concatenate(axis=3)([residual, up_conv])
-#         conv1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same')(concat)
-#         bn1 = BatchNormalization()(conv1)
-#         act1 = Activation('relu')(bn1)
-#         conv2 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same')(act1)
-#         bn2 = BatchNormalization()(conv2)
-#         act2 = Activation('relu')(bn2)
-#         return act2
-#
-#     def __unet(self, layers, input_shape, filters, num_classes):
-#         inputs = Input(shape=input_shape)
-#         residuals = []
-#         input_layer = inputs
-#
-#         for layer in xrange(layers - 1):
-#             input_layer, residual = self.__down(input_layer=input_layer, filters=filters)
-#             residuals.append(residual)
-#             filters *= 2
-#
-#         input_layer = self.__down(input_layer=input_layer, filters=filters, pool=False)
-#
-#         for layer in xrange(layers - 1):
-#             filters /= 2
-#             input_layer = self.__up(residual=residuals[-(layer + 1)], input_layer=input_layer, filters=filters)
-#
-#         outputs = Conv2D(filters=num_classes, kernel_size=(1, 1), activation='sigmoid')(input_layer)
-#
-#         model = Model(inputs=inputs, outputs=outputs)
-#         model.summary()
-#         return model
-#
-#     def create_unet_model(self):
-#         return self.__unet(layers=self.__layers, input_shape=self.__input_shape,
-#                            filters=self.__filters, num_classes=self.__num_classes)
-
-
 class UNet(object):
-    def __init__(self, layers, input_shape, filters=32, num_classes=1):
+    def __init__(self, layers, input_shape, filters, num_classes, shrink=False):
         self.__layers = layers
         self.__input_shape = input_shape
         self.__filters = filters
         self.__num_classes = num_classes
+        self.__shrink = shrink
 
     def __down(self, input_layer, filters, kernel_size=(3, 3), pool=True):
         conv1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same')(input_layer)
@@ -102,7 +42,7 @@ class UNet(object):
         act2 = Activation('relu')(bn2)
         return act2
 
-    def __unet(self, layers=6, input_shape=(320, 480, 3), filters=32, num_classes=1):
+    def __unet(self, layers=8, input_shape=(1280, 1920, 3), filters=8, num_classes=1, shrink=False):
         inputs = Input(shape=input_shape)
         residuals = []
         input_layer = inputs
@@ -119,27 +59,15 @@ class UNet(object):
             input_layer = self.__up(residual=residuals[-(layer + 1)], input_layer=input_layer, filters=filters)
 
         # --------------------------------------------------
-        up_layer = UpSampling2D(size=(2, 2))(input_layer)  # 960X640X32
-        input_us = UpSampling2D(size=(2, 2))(inputs)  # 960X640X3
-        concat = Concatenate(axis=3)([input_us, up_layer])  # 960X640X35
-        filters /= 2  # 16
-        conv1 = Conv2D(filters=filters, kernel_size=(3, 3), padding='same')(concat)  # 960X640X16
-        bn1 = BatchNormalization()(conv1)
-        act1 = Activation('relu')(bn1)
-        conv2 = Conv2D(filters=filters, kernel_size=(3, 3), padding='same')(act1)  # 960X640X16
-        bn2 = BatchNormalization()(conv2)
-        act2 = Activation('relu')(bn2)
-
-        up_layer = UpSampling2D(size=(2, 2))(act2)  # 1920X1280X16
-        input_us = UpSampling2D(size=(2, 2))(input_us)  # 1920X1280X3
-        concat = Concatenate(axis=3)([input_us, up_layer])  # 1920X1280X19
-        filters /= 2  # 8
-        conv1 = Conv2D(filters=filters, kernel_size=(1, 2), padding='valid')(concat)  # 1919X1280X8
-        bn1 = BatchNormalization()(conv1)
-        act1 = Activation('relu')(bn1)
-        conv2 = Conv2D(filters=filters, kernel_size=(1, 2), padding='valid')(act1)  # 1918X1280X8
-        bn2 = BatchNormalization()(conv2)
-        input_layer = Activation('relu')(bn2)
+        # resize to 1918X1280
+        if shrink:
+            filters /= 2
+            conv1 = Conv2D(filters=filters, kernel_size=(1, 3), padding='valid')(input_layer)  # 1918X1280
+            bn1 = BatchNormalization()(conv1)
+            act1 = Activation('relu')(bn1)
+            conv2 = Conv2D(filters=filters, kernel_size=(3, 3), padding='same')(act1)
+            bn2 = BatchNormalization()(conv2)
+            input_layer = Activation('relu')(bn2)
         # --------------------------------------------------
 
         outputs = Conv2D(filters=num_classes, kernel_size=(1, 1), activation='sigmoid')(input_layer)
@@ -150,4 +78,5 @@ class UNet(object):
 
     def create_unet_model(self):
         return self.__unet(layers=self.__layers, input_shape=self.__input_shape,
-                           filters=self.__filters, num_classes=self.__num_classes)
+                           filters=self.__filters, num_classes=self.__num_classes,
+                           shrink=self.__shrink)
