558ac3cdd4ff0893f108faddb56b013a08cd4b68
xierenqiang <840456819@qq.com>
2017-09-20 21:18:16 +0800

    fixed bugs of predict

--- cdiscount_image_classification_challenge/script/predict.py
+++ cdiscount_image_classification_challenge/script/predict.py
@@ -3,20 +3,18 @@ import time
 
 import bson
 import numpy as np
-import skimage.data
-from keras.preprocessing.image import ImageDataGenerator
 
 from config import *
-from util import *
+from util import load_model, load_img_array, get_class_indices
 
 
 def test_generator(bson_file, batch_size):
     product_cnt = 0
     pic_cnt = 0
-    num_products = 1768182
-    num_pics = 3095080
-    pics = []
+    num_products = TEST_NUM_PRODUCTS
+    num_pics = TEST_NUM_PICS
     prods = []
+    pics = []
 
     with open(bson_file, 'rb') as bf:
         data = bson.decode_file_iter(bf)
@@ -25,16 +23,16 @@ def test_generator(bson_file, batch_size):
             product_id = prod['_id']
 
             for picidx, pic in enumerate(prod['imgs']):
-                picture = skimage.data.imread(io.BytesIO(pic['picture']))
+                picture = load_img_array(io.BytesIO(pic['picture']))
                 pics.append(picture)
                 prods.append(product_id)
                 pic_cnt += 1
 
-            # guarantee pics in the same prod are in a same batch
+            # guarantee pics in the same prod are in the same batch
             if 0 <= (pic_cnt % batch_size) < 4 or pic_cnt == num_pics:
                 yield np.array(prods), np.array(pics)
-                pics = []
                 prods = []
+                pics = []
 
             product_cnt += 1
             if product_cnt % 1000 == 0 or product_cnt == num_products:
@@ -45,20 +43,7 @@ def predict_and_make_submission():
     start_time = datetime.datetime.now()
 
     model = load_model(MODEL_FILE, BEST_WEIGHTS_FILE)
-
-    train_datagen = ImageDataGenerator()
-    train_generator = train_datagen.flow_from_directory(
-        directory=TRAIN_RAW_DIR,
-        target_size=(INPUT_HEIGHT, INPUT_WIDTH),
-        batch_size=TRAIN_BATCH_SIZE,
-        class_mode='categorical',
-        seed=SEED)
-    class_indices = train_generator.class_indices()
-    dump_to_json_file(class_indices, CLASS_INDICES_FILE)
-
-    index_2_classname = {}
-    for class_name in class_indices:
-        index_2_classname[class_indices[class_name]] = class_name
+    class_2_index, index_2_class = get_class_indices(TRAIN_RAW_DIR)
 
     submission_file = SUBMISSION_DIR + "/submission-" + time.strftime("%Y%m%d%H%M%S") + ".csv"
     with open(submission_file, 'w') as outfile:
@@ -66,7 +51,7 @@ def predict_and_make_submission():
 
         product_cnt = 0
         pic_cnt = 0
-        num_products = 1768182
+        num_products = TEST_NUM_PRODUCTS
 
         for prods, pics in test_generator(TEST_BSON_FILE, PREDICT_BATCH_SIZE):
             probs = model.predict_on_batch(pics)
@@ -78,8 +63,8 @@ def predict_and_make_submission():
                     end_idx = i
                     _id = prods[start_idx]
                     prob = np.average(probs[start_idx: end_idx], axis=0)
-                    class_index = np.argmax(prob, axis=1)
-                    class_name = index_2_classname[class_index]
+                    class_index = np.argmax(prob, axis=0)
+                    class_name = index_2_class[class_index]
                     out_line = str(_id) + ',' + class_name + '\n'
                     outfile.write(out_line)
 
@@ -95,4 +80,8 @@ def predict_and_make_submission():
 
     end_time = datetime.datetime.now()
     cost_time = end_time - start_time
-    print "predict cost time:", cost_time
+    print("predict cost time: {}".format(cost_time))
+
+
+if __name__ == '__main__':
+    predict_and_make_submission()
