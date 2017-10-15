60939e364370b70b7ca1c127934275fc26970a43
xierenqiang <840456819@qq.com>
2017-10-15 22:48:06 +0800

    add fine_tune

--- cdiscount_image_classification_challenge/script/mxnet/data.py
+++ cdiscount_image_classification_challenge/script/mxnet/data.py
@@ -1,6 +1,62 @@
 import mxnet as mx
 
 
+def add_data_args(parser):
+    data = parser.add_argument_group('Data', 'the input images')
+    data.add_argument('--data-train', type=str, help='the training data')
+    data.add_argument('--data-train-idx', type=str, default='', help='the index of training data')
+    data.add_argument('--data-val', type=str, help='the validation data')
+    data.add_argument('--data-val-idx', type=str, default='', help='the index of validation data')
+    data.add_argument('--rgb-mean', type=str, default='123.68,116.779,103.939',
+                      help='a tuple of size 3 for the mean rgb')
+    data.add_argument('--pad-size', type=int, default=0,
+                      help='padding the input image')
+    data.add_argument('--image-shape', type=str,
+                      help='the image shape feed into the network, e.g. (3,224,224)')
+    data.add_argument('--num-classes', type=int, help='the number of classes')
+    data.add_argument('--num-examples', type=int, help='the number of training examples')
+    data.add_argument('--data-nthreads', type=int, default=4,
+                      help='number of threads for data decoding')
+    data.add_argument('--benchmark', type=int, default=0,
+                      help='if 1, then feed the network with synthetic data')
+    return data
+
+
+def add_data_aug_args(parser):
+    aug = parser.add_argument_group(
+        'Image augmentations', 'implemented in src/io/image_aug_default.cc')
+    aug.add_argument('--random-crop', type=int, default=1,
+                     help='if or not randomly crop the image')
+    aug.add_argument('--random-mirror', type=int, default=1,
+                     help='if or not randomly flip horizontally')
+    aug.add_argument('--max-random-h', type=int, default=0,
+                     help='max change of hue, whose range is [0, 180]')
+    aug.add_argument('--max-random-s', type=int, default=0,
+                     help='max change of saturation, whose range is [0, 255]')
+    aug.add_argument('--max-random-l', type=int, default=0,
+                     help='max change of intensity, whose range is [0, 255]')
+    aug.add_argument('--max-random-aspect-ratio', type=float, default=0,
+                     help='max change of aspect ratio, whose range is [0, 1]')
+    aug.add_argument('--max-random-rotate-angle', type=int, default=0,
+                     help='max angle to rotate, whose range is [0, 360]')
+    aug.add_argument('--max-random-shear-ratio', type=float, default=0,
+                     help='max ratio to shear, whose range is [0, 1]')
+    aug.add_argument('--max-random-scale', type=float, default=1,
+                     help='max ratio to scale')
+    aug.add_argument('--min-random-scale', type=float, default=1,
+                     help='min ratio to scale, should >= img_size/input_shape. otherwise use --pad-size')
+    return aug
+
+
+def set_data_aug_level(aug, level):
+    if level >= 1:
+        aug.set_defaults(random_crop=1, random_mirror=1)
+    if level >= 2:
+        aug.set_defaults(max_random_h=36, max_random_s=50, max_random_l=50)
+    if level >= 3:
+        aug.set_defaults(max_random_rotate_angle=10, max_random_shear_ratio=0.1, max_random_aspect_ratio=0.25)
+
+
 def get_image_iter(args, kv=None):
     image_shape = tuple([int(l) for l in args.image_shape.split(',')])
 
@@ -9,7 +65,7 @@ def get_image_iter(args, kv=None):
     else:
         (rank, nworker) = (0, 1)
 
-    train = mx.image.ImageIter(
+    train = mx.io.ImageRecordIter(
         batch_size          = args.batch_size,
         data_shape          = image_shape,
         label_width         = 1,
@@ -22,16 +78,15 @@ def get_image_iter(args, kv=None):
         # augment
         rand_crop           = True,
         rand_mirror         = True,
-        mean                = True,
+        hue                 = 0.1,
+        saturation          = 0.1,
         brightness          = 0.1,
         contrast            = 0.1,
-        saturation          = 0.1,
-        hue                 = 0.1,
         rand_gray           = 0.05,
         pca_noise           = 0.05
     )
 
-    val = mx.image.ImageIter(
+    val = mx.io.ImageRecordIter(
         batch_size          = args.batch_size,
         data_shape          = image_shape,
         label_width         = 1,
---
+++ cdiscount_image_classification_challenge/script/mxnet/fine_tune.py
@@ -0,0 +1,84 @@
+import argparse
+import logging
+
+import data
+import fit
+import mxnet as mx
+from config import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def get_fine_tune_model(symbol, arg_params, num_classes, layer_name):
+    """
+    symbol: the pre-trained network symbol
+    arg_params: the argument parameters of the pre-trained model
+    num_classes: the number of classes for the fine-tune datasets
+    layer_name: the layer name before the last fully-connected layer
+    """
+    all_layers = symbol.get_internals()
+    net = all_layers[layer_name + '_output']
+    net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc')
+    net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
+    new_args = dict({k: arg_params[k] for k in arg_params if 'fc' not in k})
+    return (net, new_args)
+
+
+if __name__ == "__main__":
+    # parse args
+    parser = argparse.ArgumentParser(description="fine-tune a dataset",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    fit.add_fit_args(parser)
+    data.add_data_args(parser)
+    data.add_data_aug_args(parser)
+
+    parser.add_argument('--pretrained-model', type=str, default='imagenet1k-resnext-101',
+                        help='the pre-trained model')
+    parser.add_argument('--layer-before-fullc', type=str, default='flatten0',
+                        help='the name of the layer before the last fullc layer')
+
+    parser.set_defaults(
+        # network
+        network             = 'resnext',
+        num_layers          = 101,
+        # data
+        data_train_imglist  = TRAIN_ALL_DIR + "/train_all_train.lst",
+        data_train_imgrec   = TRAIN_ALL_DIR + "/train_all_train.rec",
+        data_train_imgidx   = TRAIN_ALL_DIR + "/train_all_train.idx",
+        data_val_imglist    = TRAIN_ALL_DIR + "/train_all_val.lst",
+        data_val_imgrec     = TRAIN_ALL_DIR + "/train_all_val.rec",
+        data_val_imgidx     = TRAIN_ALL_DIR + "/train_all_val.idx",
+        num_classes         = NUM_CLASSES,
+        num_examples        = NUM_TRAIN_IMGS,
+        image_shape         = '3,96,96',
+        # train
+        gpus                = '0',
+        batch_size          = 16,
+        num_epochs          = 10,
+        lr                  = 0.01,
+        lr_factor           = 0.1,
+        lr_step_epochs      = '5',
+        wd                  = 0,
+        mom                 = 0,
+        optimizer           = 'sgd',
+        disp_batches        = 10,
+        model_prefix        = MODEL_DIR + "/resnext101",
+    )
+
+    # use less augmentations for fine-tune
+    data.set_data_aug_level(parser, 1)
+
+    args = parser.parse_args()
+
+    # pre-trained model should be downloaded and renamed
+    sym, arg_params, aux_params = mx.model.load_checkpoint(MODEL_DIR + "/resnext101", 1)
+
+    # remove the last fullc layer
+    (new_sym, new_args) = get_fine_tune_model(sym, arg_params, args.num_classes, args.layer_before_fullc)
+
+    # train
+    fit.fit(args        = args,
+            network     = new_sym,
+            data_loader = data.get_image_iter,
+            arg_params  = new_args,
+            aux_params  = aux_params)
